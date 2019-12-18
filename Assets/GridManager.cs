27c9f42ab014fb43548d220bc63d3bd6281df562
Spejderman <r.odgaard@gmail.com>
2019-12-18 18:55:35 +0100

    Full screen grid working

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -11,28 +11,44 @@ public class GridManager : MonoBehaviour
     Vector3 max;
 
     Vector3 topLeft, topRight, bottomLeft, bottomRight;
+    private Vector2 gridSize;
+
     void Start()
     {
         min = Camera.main.ScreenToWorldPoint(Vector3.zero);
         max = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
+        Vector2 sideLength = new Vector2(Mathf.Abs(max.x - min.x), Mathf.Abs(max.y - min.y));
+        float spriteSideLength = prefab.GetComponent<SpriteRenderer>().bounds.size.x;
+
+
+
+        gridSize = new Vector2(Screen.width / 16, Screen.height / 16);
+
         topRight = max + new Vector3(0, 0, 10);
         topLeft = new Vector3(min.x, max.y, 0);
         bottomLeft = min + new Vector3(0, 0, 10);
         bottomRight = new Vector3(max.x, min.y, 0);
-        print("tr: " + topRight);
-        print("tl: " + topLeft);
-        print("bl: " + bottomLeft);
-        print("br: " + bottomRight);
+
+        SpriteRenderer sr = prefab.GetComponent<SpriteRenderer>();
+        double width = sr.sprite.bounds.size.x;
+        Debug.Log("width: " + width);
+        double worldScreenHeight = Camera.main.orthographicSize * 2.0;
+        double worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
 
 
 
-        float side = Mathf.Abs(min.x - max.x);
-        float scale = side / prefab.GetComponent<SpriteRenderer>().bounds.size.x;
-        print("side: " + side + " scale: " + scale);
-        for (float i = min.x; i <= max.x; i += scale)
+        GameObject go = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        go.transform.localScale = Vector2.one * (float)(worldScreenWidth / width) / gridSize.x;
+        float tileSize = (float)(worldScreenWidth / width) / gridSize.x;
+        go.transform.localScale *= 0.9f;
+
+        for (float y = 0; y < gridSize.y; y++)
         {
-            Instantiate(prefab, new Vector3(i + (scale / 2), 0.0f, 0.0f), Quaternion.identity);
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                Instantiate(go, new Vector3(min.x + (sideLength.x / gridSize.x) * 0.5f + (sideLength.x / gridSize.x * x), min.y + (sideLength.x / gridSize.x) * 0.5f + (sideLength.y / gridSize.y * y), 0), Quaternion.identity);
+            }
         }
     }
 
